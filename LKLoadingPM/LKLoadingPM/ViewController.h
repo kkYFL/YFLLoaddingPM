79647ad4fe57dbb90a69b1afed047630a5f13525
杨丰林 <yangfenglin@mistong.com>
2017-08-08 17:22:23 +0800

    初始化项目

---
+++ LKLoadingPM/LKLoadingPM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKLoadingPM
+//
+//  Created by 杨丰林 on 2017/8/8.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LKLoadingPM/LKLoadingPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LKLoadingPM
+//
+//  Created by 杨丰林 on 2017/8/8.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
