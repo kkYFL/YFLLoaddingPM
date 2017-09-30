5b0de3e80805994939e0e05adba4270468cbd241
杨丰林 <1490719714@qq.com>
2017-09-30 11:35:53 +0800

    见面搭建修改

--- LKLoadingPM/Controller/ViewController.m
+++ LKLoadingPM/Controller/ViewController.m
@@ -16,22 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.view.backgroundColor = [UIColor colorWithRed:238/255.0 green:224/255.0 blue:229/255.0 alpha:1];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
