123172379c91e816d747406aec0f5cae39817e2a
杨丰林 <1490719714@qq.com>
2018-02-18 22:53:58 +0800

    添加下载文件数据模型

--- LKLoadingPM/Model/TaskLoadModel.h
+++ LKLoadingPM/Model/TaskLoadModel.h
@@ -7,7 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface TaskLoadModel : NSObject
+typedef NS_ENUM(NSInteger,DownLoadState) {
+    DownloadStateStart,          /**下载开始**/
+    DownLoadStateSuspended,      /**下载暂停**/
+    DownLoadStateCompleted,      /**下载完成**/
+    DownLoadStateFailed          /**下载失败**/
+};
 
+typedef void(^YFLDownloadProgressBlock)(CGFloat progresss,NSString *speed,NSString *remainTime,NSString *writtenSize,NSString *totalSize);
+typedef void(^YFLDownloadStateBlock)(DownLoadState state);
+
+@interface TaskLoadModel : NSObject <NSCoding>
+@property (nonatomic, strong) NSOutputStream *stream;   //流
+@property (nonatomic, copy) NSString *url;              //下载地址
+@property (nonatomic, strong) NSDate *startTime;        //下载开始时间
+@property (nonatomic, copy) NSString *fileName;         //文件名
+@property (nonatomic, copy) NSString *totalSize;        //文件大小
+@property (nonatomic, assign) NSInteger totalLength;    //获取服务器这次请求，返回数据总长度
+
+
+@property (nonatomic, copy) YFLDownloadProgressBlock progressBlock; //下载进度回调
+@property (nonatomic, copy) YFLDownloadStateBlock stateBlock;       //下载状态回调
+
+
+-(float)calculateFileSizeInUnit:(unsigned long long)contentLength;//计算文件大小方法
+-(NSString *)caculateUnit:(unsigned long long)contentLength;      //计算文件大小单位方法
 @end
--- LKLoadingPM/Model/TaskLoadModel.m
+++ LKLoadingPM/Model/TaskLoadModel.m
@@ -10,4 +10,46 @@
 
 @implementation TaskLoadModel
 
+-(float)calculateFileSizeInUnit:(unsigned long long)contentLength{
+    if (contentLength >= pow(1024, 3)) {return (float) (contentLength / (float) pow(1024, 3));}
+    else if (contentLength >= pow(1024, 2)) {return (float) (contentLength / (float) pow(1024, 2));}
+    else if (contentLength >= 1024){ return (float) (contentLength / (float) 1024);}
+    else {return (float) (contentLength);}
+}
+
+
+-(NSString *)caculateUnit:(unsigned long long)contentLength{
+    if (contentLength >= pow(1024, 3)) {
+        return @"GB";
+    }else if (contentLength >= pow(1024, 2)){
+        return @"MB";
+    }else if (contentLength >= 1024){
+        return @"KB";
+    }else{
+        return @"B";
+    }
+}
+
+
+//将属性进行编码
+-(void)encodeWithCoder:(NSCoder *)aCoder{
+    [aCoder encodeObject:self.url forKey:@"url"];
+    [aCoder encodeObject:self.fileName forKey:@"fileName"];
+    [aCoder encodeInteger:self.totalLength forKey:@"totalLength"];
+    [aCoder encodeObject:self.totalSize forKey:@"totalSize"];
+}
+
+
+//将属性进行解码
+-(instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super init];
+    if (self) {
+        self.url = [aDecoder decodeObjectForKey:@"url"];
+        self.fileName = [aDecoder decodeObjectForKey:@"fileName"];
+        self.totalLength = [aDecoder decodeIntegerForKey:@"totalLength"];
+        self.totalSize = [aDecoder decodeObjectForKey:@"totalSize"];
+    }
+    return self;
+}
+
 @end
