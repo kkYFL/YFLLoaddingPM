29348f660e606df57e3b2580ae36affb71fe98c2
杨丰林 <1490719714@qq.com>
2018-02-25 18:04:14 +0800

    管理者修改

--- LKLoadingPM/Model/TaskLoadingManager.h
+++ LKLoadingPM/Model/TaskLoadingManager.h
@@ -7,7 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TaskLoadModel.h"
+
+//缓存主目录
+#define YFLCachesDirectory [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"YFLCache"]
+//文件名
+#define YFLFileName(url) [[url componentsSeparatedByString:@"/"] lastObject]
+
+//文件存放路径（caches）
+#define YFLFileFullPath(url) [YFLCachesDirectory stringByAppendingPathComponent:YFLFileName(url)]
+//文件已下载长度
+#define YFLDownloadLength(url) [[[NSFileManager defaultManager] attributesOfItemAtPath:YFLFileFullPath(url) error:nil][NSFileSize] integerValue]
+//储存文件信息的路径（caches）
+#define YFLDownloadDetailPath [YFLCachesDirectory stringByAppendingPathComponent:@"downloadDetail.data"]
+
 
 @interface TaskLoadingManager : NSObject
 
+/**
+ * 返回单例对象
+ */
+
++(instancetype)sharedInstance;
+
+/**
+ * 开启任务下载资源
+ * @param url              下载地址
+ * @param progresssBlock   下载进度回调
+ * @param stateBlock       下载状态
+ */
+
+-(void)download:(NSString *)url progress:(YFLDownloadProgressBlock)progressBlock state:(YFLDownloadStateBlock)stateBlock;
 @end
--- LKLoadingPM/Model/TaskLoadingManager.m
+++ LKLoadingPM/Model/TaskLoadingManager.m
@@ -8,6 +8,147 @@
 
 #import "TaskLoadingManager.h"
 
+@interface TaskLoadingManager ()
+
+@property (nonatomic, strong) NSMutableArray *sessionModelsArray;   //所有下载数据模型数组
+
+@end
+
 @implementation TaskLoadingManager
+#pragma mark - 接口
+static TaskLoadingManager *_downloadManager;
++(instancetype)sharedInstance{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _downloadManager = [[TaskLoadingManager alloc]init];
+    });
+    return _downloadManager;
+}
+
+-(void)download:(NSString *)url progress:(YFLDownloadProgressBlock)progressBlock state:(YFLDownloadStateBlock)stateBlock{
+    if (!url) {
+        return;
+    }
+
+    if (self) {
+        
+    }
+}
+
+#pragma mark - 内部方法
+
+/**
+ * 判断该文件是否下载完成
+ *
+ */
+
+-(BOOL)isCompletion:(NSString *)url{
+//    if ([self fileTotalLength:url] && YFLDownloadLength(url) == [self fileTotalLength:url]) {
+//        return YES;
+//    }
+    if ([self fileTotalLength:url] && YFLDownloadLength(url) == [self fileTotalLength:url]) {
+        return YES;
+    }
+    return NO;
+}
+
+/**
+ * 获取该资源大小
+ */
+-(NSInteger)fileTotalLength:(NSString *)url{
+    for (TaskLoadModel *model in self.sessionModelsArray) {
+        if ([model.url isEqualToString:url]) {
+            return model.totalLength;
+        }
+    }
+    return 0;
+}
+
+/**
+ * 读取model
+ */
+-(NSArray *)getSessionsModels{
+    // 文件信息
+    NSArray *sessionModels = [NSKeyedUnarchiver unarchiveObjectWithFile:YFLDownloadDetailPath];
+    return sessionModels;
+}
+
+
+
+
+
+
+
+
+
+#pragma mark - 懒加载
+-(NSMutableArray *)sessionModelsArray{
+    if (!_sessionModelsArray) {
+        _sessionModelsArray = @[].mutableCopy;
+        [_sessionModelsArray addObjectsFromArray:[self getSessionsModels]];
+    }
+    return _sessionModelsArray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
