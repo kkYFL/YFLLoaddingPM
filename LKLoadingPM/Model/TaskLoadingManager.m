7b4c70d2faaca34311b0179e0c7152812213c2be
杨丰林 <1490719714@qq.com>
2018-02-26 23:30:14 +0800

    完善下载管理者

--- LKLoadingPM/Model/TaskLoadingManager.m
+++ LKLoadingPM/Model/TaskLoadingManager.m
@@ -11,6 +11,12 @@
 @interface TaskLoadingManager ()
 
 @property (nonatomic, strong) NSMutableArray *sessionModelsArray;   //所有下载数据模型数组
+/** 保存所有下载相关信息字典 */
+@property (nonatomic, strong) NSMutableDictionary *sessionModels;
+/** 保存所有下载任务(注：key:value = 文件名：下载任务) */
+@property (nonatomic, strong) NSMutableDictionary *tasks;
+
+
 
 @end
 
@@ -25,14 +31,29 @@ static TaskLoadingManager *_downloadManager;
     return _downloadManager;
 }
 
+
+
+/**
+ *
+ * 开启任务下载资源
+ */
 -(void)download:(NSString *)url progress:(YFLDownloadProgressBlock)progressBlock state:(YFLDownloadStateBlock)stateBlock{
-    if (!url) {
+    
+    if (!url) return;
+
+    //下载完成
+    if ([self isCompletion:url]) {
+        stateBlock(DownLoadStateCompleted);
+        NSLog(@"--该资源已经下载完成");
         return;
     }
-
-    if (self) {
+    
+    //暂停
+    if ([self.tasks valueForKey:YFLFileName(url)]) {
         
+        return;
     }
+    
 }
 
 #pragma mark - 内部方法
@@ -43,9 +64,6 @@ static TaskLoadingManager *_downloadManager;
  */
 
 -(BOOL)isCompletion:(NSString *)url{
-//    if ([self fileTotalLength:url] && YFLDownloadLength(url) == [self fileTotalLength:url]) {
-//        return YES;
-//    }
     if ([self fileTotalLength:url] && YFLDownloadLength(url) == [self fileTotalLength:url]) {
         return YES;
     }
@@ -73,11 +91,55 @@ static TaskLoadingManager *_downloadManager;
     return sessionModels;
 }
 
+-(void)handle:(NSString *)url{
+    NSURLSessionDataTask *task = [self getTask:url];
+    //暂停
+    if (task.state == NSURLSessionTaskStateRunning) {
+        [self pause:url];
+    //开始
+    }else{
+        [self start:url];
+    }
+    
+    //创建缓存目录文件
+    
+}
 
+/**
+ * 根据url获得对应的下载任务
+ */
+-(NSURLSessionDataTask *)getTask:(NSString *)url{
+    return (NSURLSessionDataTask *)[self.tasks valueForKey:YFLFileName(url)];
+}
 
+/**
+ * 暂停下载
+ */
+-(void)pause:(NSString *)url{
+    NSURLSessionDataTask *task = [self getTask:url];
+    //暂停
+    [task suspend];
+    //暂停回调
+    [self getSessionModel:task.taskIdentifier].stateBlock(DownLoadStateSuspended);
+}
 
+/**
+ *  根据url获取对应的下载信息模型
+ */
+- (TaskLoadModel *)getSessionModel:(NSUInteger)taskIdentifier{
+    return (TaskLoadModel *)[self.sessionModels valueForKey:@(taskIdentifier).stringValue];
+}
 
-
+/**
+ * 开始下载
+ */
+-(void)start:(NSString *)url{
+    NSURLSessionDataTask *task = [self getTask:url];
+    //恢复下载
+    [task resume];
+    //状态回调
+    [self getSessionModel:task.taskIdentifier].stateBlock(DownloadStateStart);
+}
 
 
 
@@ -90,6 +152,19 @@ static TaskLoadingManager *_downloadManager;
     return _sessionModelsArray;
 }
 
+-(NSMutableDictionary *)tasks{
+    if (!_tasks) {
+        _tasks = [NSMutableDictionary dictionary];
+    }
+    return _tasks;
+}
+
+-(NSMutableDictionary *)sessionModels{
+    if (!_sessionModels) {
+        _sessionModels = @{}.mutableCopy;
+    }
+    return _sessionModels;
+}
 
 
 
